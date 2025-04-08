2313e4a23e5817c44f309aad08b5b7ee534693cc
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-08 13:56:13 +0200

    adding celestial bodies to the scene

--- src/components/SpaceScene.vue
+++ src/components/SpaceScene.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-console -->
 <script setup lang="ts">
 import * as THREE from 'three'
 import { onBeforeUnmount, onMounted, ref } from 'vue'
@@ -11,11 +12,99 @@ let camera: THREE.PerspectiveCamera
 let renderer: THREE.WebGLRenderer
 let animationFrameId: number
 
+// # Celestials variables
+interface CelestialBody {
+  mesh: THREE.Mesh
+  orbit: number
+  speed: number
+  rotationSpeed: number
+  name: string
+}
+
+let planets: CelestialBody[] = []
+let sun: THREE.Mesh
+
+// # Solar system function
+const createSolarSystem = () => {
+  // * Sun
+  const sunGeometry = new THREE.SphereGeometry(5, 32, 32)
+  const sunMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFAA00, // TODO: > VAR this color
+    // ! BROKEN: CANT FIND RIGHT PROPERTY TO EMIT LIGHT
+    // TODO: <!> Need digging into LightMap & LightMapIntensity (probably)
+    emissive: 0xFFAA00,
+    emissiveIntensity: 1
+  })
+  sun = new THREE.Mesh(sunGeometry, sunMaterial)
+  scene.add(sun)
+  
+  const sunLight = new THREE.PointLight(0xFFFFFF, 1.5, 1000) // TODO: > VAR these
+  sun.add(sunLight)
+  
+  // * Planets
+  const planetColors = [
+    0x3498DB, // blue (Mercury?)
+    0xE67E22, // orange (Venus?)
+    0x2ECC71, // green (Earth?)
+    0xE74C3C, // red (Mars?)
+    0xF1C40F, // yellow (Jupiter?)
+    0x9B59B6, // purple (Saturn?)
+    0x1ABC9C, // teal (Uranus?)
+    0x34495E, // d-blue (Neptune?)
+  ]
+  
+  const planetNames = [
+    'Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune'
+  ]
+  
+  // * Set unique stats for each planet
+  for (let i = 0; i < 8; i++) {
+    // ** Size
+    const size = i < 4 ? 0.8 + (i * 0.2) : 1.5 + ((i - 4) * 0.3)
+
+    // ** Distance to Sun
+    const orbit = 10 + (i * 6)
+
+    // ** Orbiting speed
+    const speed = 0.01 / (i * 0.1 + 1)
+
+    // ** Rotation speed
+    const rotationSpeed = 0.01 + (Math.random() * 0.02)
+
+    // ** Geometry & Material
+    const planetGeometry = new THREE.SphereGeometry(size, 24, 24)
+    const planetMaterial = new THREE.MeshStandardMaterial({
+      color: planetColors[i],
+      roughness: 0.7,
+      metalness: 0.3,
+    })
+
+    // ** Mesh
+    const planet = new THREE.Mesh(planetGeometry, planetMaterial)
+
+    // ** Initial orbit position
+    const angle = Math.random() * Math.PI * 2
+    planet.position.x = Math.cos(angle) * orbit
+    planet.position.z = Math.sin(angle) * orbit
+
+    scene.add(planet)
+
+    // ** Animation data
+    planets.push({
+      mesh: planet,
+      orbit,
+      speed,
+      rotationSpeed,
+      name: planetNames[i],
+    })
+  }
+}
+
 // # Init function
 function initScene() {
   console.log('scene is initializing...')
   if (!sceneContainer.value) {
-    // console.log('container not found!')
+    // console.error('container not found!')
     return
     // console.log('container found, dimensions:', sceneContainer.value.clientWidth, sceneContainer.value.clientHeight)
   }
@@ -25,18 +114,19 @@ function initScene() {
 
   // * Scene building
   scene = new THREE.Scene()
-  scene.background = new THREE.Color(0x000000) // todo: > VAR this color
+  scene.background = new THREE.Color(0x000000) // TODO: > VAR this color
 
   // * Adding ambient light
-  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
+  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5) // TODO: > VAR this color
   scene.add(ambientLight)
 
   // * Adding stars to the background
-  // todo: >> find another way to implement real space
+  // TODO: >> Find another way to implement real space
   const starsGeometry = new THREE.BufferGeometry()
   const starsMaterial = new THREE.PointsMaterial({
-    color: 0xFFFFFF, // todo: > VAR this color
+    color: 0xFFFFFF, 
     size: 0.5,
+    sizeAttenuation: true
   })
 
   const starsVertices = []
@@ -51,6 +141,9 @@ function initScene() {
   const stars = new THREE.Points(starsGeometry, starsMaterial)
   scene.add(stars)
 
+  // * Solar system building
+  createSolarSystem()
+
   // ? DEBUG: adding a test cube (red)
   /*   const testGeometry = new THREE.BoxGeometry(10, 10, 10)
   const testMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000 })
@@ -60,8 +153,8 @@ function initScene() {
   console.log('test cube added') */
 
   // * Camera building
-  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 2000)
-  camera.position.z = 50
+  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 2000) // TODO: > VAR these
+  camera.position.z = 50 // TODO: > VAR this
   // console.log('camera created & positioned @: ', camera.position)
 
   // * Renderer building
@@ -78,7 +171,7 @@ function initScene() {
   }
 
   // ? DEBUG: explict call after first render
-  // //renderer.render(scene, camera)
+  // renderer.render(scene, camera)
   // console.log('first render executed')
 
   // * Resizing handler
